e49bc6b4a363284dd3011fb5f79b6c26ae3669f5
김준호 <enddl0121@gmail.com>
2020-08-07 16:13:35 +0900

    보일러플레이트

--- boilerPlate/client/src/index.js
+++ boilerPlate/client/src/index.js
@@ -5,11 +5,36 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+//redux에서 미들웨어를 쓸 수 있게 해주는거랑 스토어만들어주는거
 
+import { Provider } from 'react-redux'
+//redux를 연결시켜주기 위해서는 redux에서 제공하는 provider를 사용해야한다.
+
+
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+
+import Reducer from './_reducers/index'
+//import들은 body보다 무조건 위에 있어야한다 = const create 아래에 있으면 안됨
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
+//리덕스 미들웨어를 이용해서 객체의 액션만 받는 리덕스 스토어에
+//프로미스 형식과 함수도 받게 할 수 있다.
+
+
+
+
+//Redux_devtools를 연결하기위해서 Reducer, 위해서 사용함
+//redux_devtools는 크롬확장프로그램에서 다운가ㅡㄴㅇ
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider
+      store={createStoreWithMiddleware(Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__())}
+    >
+      <App />,
+    </Provider>,
   document.getElementById('root')
 );
 
