abc29b8d488a4cabed473f9d5998e41cc73af471
김준호 <enddl0121@gmail.com>
2020-08-10 13:54:16 +0900

    무비앱

---
+++ MovieApp/client/src/components/views/RegisterPage/RegisterPage.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../_actions/user_action";
+
+import { withRouter} from 'react-router-dom'
+//props.history withrouter를 이용한다.
+
+function RegisterPage(props) {
+
+  //redux를 사용하기 위한 dispatch
+  const dispatch = useDispatch();
+
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [Name, setName] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  }
+
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value);
+  }
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  }
+
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value);
+  }
+
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+
+
+    if(Password !== ConfirmPassword){
+      return alert('비밀번호와 비밀번호 확인은 같아야 합니다.')
+    }
+      
+    let body = {
+      email: Email,
+      name : Name,
+      password: Password,
+    };  
+    //ConfirmPassword는 데이터베이스로 보내지 않기 때문에 body x
+
+            //위에서 import한  registerUser라는 함수를 body에 가지고
+            //registerUser라는 함수액션 //user_action에 있는 loginUser에 body
+  dispatch(registerUser(body))
+  .then(response =>{
+    console.log('client 리스폰 ',response)
+    console.log('clinet props' , props)
+    if(response.payload.success){
+      alert("회원가입에 성공하였습니다.")
+      props.history.push('/login')
+    } else {
+      alert('회원가입에 실패하였습니다.')
+    }
+  })
+};
+  
+
+
+
+
+  return (
+    <div style={{
+      display: 'flex', justifyContent: 'center', alignItems:'center'
+      ,width: '100%', height: '100vh'
+    }}>
+      <form style={{display:'flex', flexDirection: 'column'}}
+          onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler}/>
+
+        <label>Name</label>
+        <input type="text" value={Name} onChange={onNameHandler}/>
+
+        <label>PassWord</label>
+        <input type="password" value={Password} onChange={onPasswordHandler}/>
+
+        <label>Confirm PassWord</label>
+        <input type="password" value={ConfirmPassword} onChange={onConfirmPasswordHandler}/>
+        
+        <br/>
+
+        <button type = "submit">
+          회원 가입
+        </button>
+      </form>
+      
+    </div>
+  )
+}
+
+export default withRouter(RegisterPage)
---
+++ MovieApp/server/middleware/auth.js
@@ -0,0 +1,31 @@
+const { User } = require('../models/User')
+
+let auth = (req, res, next) => {
+  console.log(req.cookies)
+  console.log("0번 클라이언트에 있는 토큰: ",req.cookies.x_auth)
+  //인증처리를 하는곳
+  //client의 쿠키에저 가져온 x_auth가 사라지는건 아니다.
+  //종료 할떄 생김
+
+  // client 쿠키에서 토큰을 가져온다.
+  let token = req.cookies.x_auth;
+
+  // 토큰을 복호화 한다음에 데이터 베이스에서 유저를 찾는다.
+  //findeByToken은 만들어준 메소드
+  User.findByToken(token, (err,user)=> {
+    console.log('2번 auth token: ', token)
+    console.log('3번 auth user: ', user)
+    if(err) throw err;
+    if(!user) return res.json({ isAuth: false, error: true })
+
+    //유저와 토큰정보를 req에 넣어주어야 그 뒤에 사용가능
+    req.token = token;
+    req.user = user;
+    console.log('4번 auth req.token: ', req.token)
+    console.log('5번 auth req.user: ', req.user)
+    next()//next를 하는 이유는 미들웨어에서 다음으로 가게 하기 위해서
+  })
+}
+
+module.exports = { auth }
+//auth안에서 인증처리를 한다.
